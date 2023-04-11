900bd538c6a7a6c2973266e0a190cc9d63a240d7
Damian1255 <damianlau96@gmail.com>
2023-04-12 00:48:42 +0800

    v2

---
+++ spam_RNN_v2.py
@@ -0,0 +1,27 @@
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+# Load the model
+model = load_model('Models/model_RNN_v2.h5')
+
+while True:
+    # Get input text for prediction
+    text = input('Enter your message: ')
+
+    # Preprocess the text
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts([text])
+    sequences = tokenizer.texts_to_sequences([text])
+    X = pad_sequences(sequences, maxlen=50)
+
+    # Prediction
+    y_pred = model.predict([X, np.zeros((1,3))], verbose=0)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    # Print result
+    if y_pred == 1:
+        print('Likely Spam\n')
+    else:
+        print('Unlikely Spam\n')
